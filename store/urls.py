b6385f794cca301ea8cdf11a4baf648aa4bd5d30
hodakoov <teeror93@gmail.com>
2023-01-08 11:40:19 +0400

    1. Добавлен свой urls.py в приложении products и сделан include в проектном urls. 2. Добавлено приложение Users. Оно изменяет стандарную модель User.

--- store/urls.py
+++ store/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from products import views
 
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
+    path('products/', include('products.urls')),
 ]
 
 #для локальной разработки
