947698b9064337282c932107c69dbae1225ce694
hodakoov <teeror93@gmail.com>
2023-01-10 20:42:18 +0400

    Добавлен личный кабинет (профиль). В файлах base.html, urls.py добавлены ссылки на профиль. В profile.html сформирован шаблон (база взята из приложения продуктс). Все формы заполнения взяты из forms.py. В forms.py сформирован класс под профиль. В views.py cформирована контроллер для изменения данных и для отображения текущих. Доп.пояснения: 1. forms.py - image - required=False - не обязательное поля к заполнению. 2. forms.py - username, email - 'readonly': True - нельзя изменять в личном кабинете, сырым выделены. 3. views.py -  profile - form - параметры у UserProfileForm [     instance=request.user - для заполнения пустых полей в форме. Передача экземпляра объекта;     data=request.POST - обноленные данные, которые ввели;     files=request.FILES - передача картинки в переменную FILES для последующей загрузки. Работает совместно с п.4 4. profile.html - form - enctype="multipart/form-data" - для загрузки картинок.

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-from .forms import UserLoginForm, UserRegistrationForm
+from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -30,3 +30,15 @@ def registration(request):
     form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context)
