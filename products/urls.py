49febc9d73a1e4562db09f1911e25a56d800b3fa
hodakoov <teeror93@gmail.com>
2023-01-10 11:23:57 +0400

    1. В приложении products в шаблонах base and index добавлены динамические урл относительно приложения. Также в index  добавлен url на шаблон логина из приложения users. 2. В связи с изменениями в п.1 расширены urls в store and products. 3. В приложение users добавлены шаблоны на страницу авторизации и страницу регистрации. Добавлены ссылки в urls. 4. ФОРМЫ. Создан forms.py, в котором описана форма заполнения логина и пароля. p.s. Изначально для примера в шаблоне был использован -> {{ form.as_p }}. Отображалось то, что было прописана в fiels, class MEta, class UserLoginFOrm. Далее Каждый из полей преобразован к виду который был изначально в шаблоне, т.е. взят нужный класс и плейсхолдер. В связи с этим в шаблоне были переписаны строки отвечающие за отбражение строки ввода логина и пароля. 5. ВО vies.py реализован контроль авторизации. Зайдейстован метод POST. Сначала проверяется какой метод поступает, далее идет проверка на валидность заполненной формы, а далее проверка на получение разрешения на вход.

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from products import views
 
+
+app_name = 'products'
 urlpatterns = [
-    path('', views.products, name='products')
+    path('', views.products, name='index')
 ]
