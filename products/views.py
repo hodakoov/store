3a301f8fc508b717656aa9574e8acf74eda3fa94
hodakoov <teeror93@gmail.com>
2023-01-13 17:04:34 +0400

    Добавлены Контроллеры - обработчики событий Контроллер обработчик кнопки добавления товара и удаления товара из корзины по крестику. в шаблоне baskets.html добавлен цикл для прохождения по элементам корзины и отображения в профиле.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
+from itertools import product
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Product, ProductCategory
+from .models import Product, ProductCategory, Baskets
 
 
 def index(request):
@@ -16,3 +19,23 @@ def products(request):
         'categories': ProductCategory.objects.all()
     }
     return render(request, 'products/products.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Baskets.objects.filter(product=product, user=request.user)
+
+    if not baskets.exists():
+        Baskets.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, basket_id):
+    basket = Baskets.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
