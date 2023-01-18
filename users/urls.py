bd49c5584cff1d7dacef6f642372af30f0f938be
hodakoov <teeror93@gmail.com>
2023-01-18 09:48:22 +0400

    Добавлен класс для Login. А для выхода испортирован Logout сразу в urls. В settings.py добавлены 2 глобальных редиректа.

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
 from users import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('login', views.UserLoginView.as_view(), name='login'),
     path('registration', views.UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>/', login_required(views.UserProfileView.as_view()), name='profile'),
-    path('logout', views.logout, name='logout'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,27 +1,15 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
 
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Baskets
 from users.models import User
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
 
 class UserRegistrationView(CreateView):
@@ -48,37 +36,3 @@ class UserProfileView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
-
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
-
-
-
-# def registration(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Вы успешно зарегистировались.')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegistrationForm()
-#     context = {'form': form}
-#     return render(request, 'users/register.html', context)
-
-
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#     else:
-#         form = UserProfileForm(instance=request.user)
-#     context = {'title': 'Store - Профиль',
-#                'form': form,
-#                'baskets': Baskets.objects.filter(user=request.user)}
-#     return render(request, 'users/profile.html', context)
