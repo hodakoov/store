6e925e841015bf64a3540b3d63461cddcbfa805c
hodakoov <teeror93@gmail.com>
2023-01-24 16:02:12 +0400

    Создание тестов для главной страницы сайта. проверка статус кода, title и шаблона.

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
 
-# Create your tests here.
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Магазин')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+
