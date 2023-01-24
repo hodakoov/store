c00cec9352d830a5aa94217581cc08efe3a16f6b
hodakoov <teeror93@gmail.com>
2023-01-24 16:36:24 +0400

    Добавлены тесты для страницы с продуктами и категорией продуктов. Проверка статус кода, title и шаблона вынесены в отдельный метод, т.к. повторяются. + проверяется вывод данных продуктов на странице.

--- products/tests.py
+++ products/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from http import HTTPStatus
 
+from products.models import Product, ProductCategory
+
 
 class IndexViewTestCase(TestCase):
 
@@ -14,3 +16,32 @@ class IndexViewTestCase(TestCase):
         self.assertTemplateUsed(response, 'products/index.html')
 
 
+class ProductListViewTestCase(TestCase):
+    fixtures = ['category.json', 'products.json']
+
+    def setUp(self):
+        self.products = Product.objects.all()
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self._common_tests(response)
+
+        self.assertEqual(list(response.context_data['object_list']),
+                         list(self.products[:3]))
+
+    def text_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'categody_id': category.id})
+        response = self.client.get(path)
+
+        self._common_tests(response)
+
+        self.assertEqual(list(response.context_data['object_list']),
+                         list(self.products.filter(category_id=category.id)))
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Магазин -  Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
