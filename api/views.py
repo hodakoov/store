39eef227e2578026f5d30b26d7daf4f5602b8a70
hodakoov <teeror93@gmail.com>
2023-02-21 16:10:28 +0400

    Добавлен API корзины пользователя. Отображаются все данные что и в профиле.

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-from products.models import Product
-from products.serializers import ProductSerializer
+from products.models import Product, Baskets
+from products.serializers import ProductSerializer, BasketSerializer
 
 
 class ProductModelViewSet(ModelViewSet):
@@ -13,5 +13,17 @@ class ProductModelViewSet(ModelViewSet):
         if self.action in ('create', 'update', 'partial_update', 'destroy'):
             self.permission_classes = (IsAdminUser,)
         return super(ProductModelViewSet, self).get_permissions()
-        
+
+
+class BasketModelViewSet(ModelViewSet):
+    queryset = Baskets.objects.all()
+    serializer_class = BasketSerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = None
+
+    def get_queryset(self):
+        # функция для отображения продуктов в корзине для конкретно авторизированного пользователя
+        # т.к. выше мы можем определить только для всей корзины (queryset), без выборки по пользователю
+        queryset = super(BasketModelViewSet, self).get_queryset()
+        return queryset.filter(user=self.request.user)
 
--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Baskets
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -12,3 +12,21 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category',)
+
+
+class BasketSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    sum = serializers.FloatField()
+    total_sum = serializers.SerializerMethodField()
+    total_quantity = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Baskets
+        fields = ('id', 'product', 'quantity', 'sum', 'total_sum', 'total_quantity', 'created_timestamp',)
+        read_only_fields = ('created_timestamp',)
+
+    def get_total_sum(self, obj):
+        return Baskets.objects.filter(user_id=obj.user.id).total_sum()
+
+    def get_total_quantity(self, obj):
+        return Baskets.objects.filter(user_id=obj.user.id).total_quantity()
