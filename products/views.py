3e9066ca68da5173827d3166e41f6c829fe348b1
hodakoov <teeror93@gmail.com>
2023-03-15 09:24:39 +0400

    Починка пагинации с использованием кэша(редис)

--- products/views.py
+++ products/views.py
@@ -22,6 +22,7 @@ class ProductListView(TitleMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductListView, self).get_context_data()
         context['categories'] = ProductCategory.objects.all()
+        context['category_id'] = self.kwargs.get('category_id')
         return context
 
     def get_queryset(self):
